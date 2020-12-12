38a7fb8f68eaa8cc35c8d831ef2626869d0eb9d3
Rithvik Nishad <rithvikn2001@gmail.com>
2020-12-12 11:49:22 +0530

    chng bakecode-jobs to bakecode

--- lib/bakecode-jobs.dart
+++
@@ -1,7 +0,0 @@
-library jobs;
-
-export 'src/flow_context.dart';
-export 'src/flow.dart';
-export 'src/node.dart';
-
-// TODO: add uuid generation during connectTo()
---
+++ lib/bakecode.dart
@@ -0,0 +1,9 @@
+library bakecode;
+
+import 'dart:async';
+
+import 'package:meta/meta.dart';
+
+part 'src/flow.dart';
+part 'src/flow_context.dart';
+part 'src/provider.dart';
