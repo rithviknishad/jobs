b59bd38de3a2a516adc17c0b50aeab70e3408296
Rithvik Nishad <rithvikn2001@gmail.com>
2020-12-12 15:55:03 +0530

    update example to include recipe

--- example/example.dart
+++ example/example.dart
@@ -3,13 +3,21 @@ import 'dart:async';
 import 'package:bakecode/bakecode.dart';
 import 'package:meta/meta.dart';
 
-Future<void> main(List<String> args) async {
-  await runFlow(MyFlow());
+Future<void> main(List<String> args) => runFlow(MyRecipe());
+
+class MyRecipe extends Flow {
+  @override
+  Flow run(FlowContext context) {
+    return Recipe(
+      name: 'MyRecipe',
+      startsFrom: MyFlow(),
+    );
+  }
 }
 
 class MyFlow extends Flow {
   @override
-  FutureOr<Flow> run(FlowContext context) {
+  Flow run(FlowContext context) {
     return ParallelFlow(
       flows: [
         DelayedPrinterJob(),
@@ -27,7 +35,7 @@ class MyFlow extends Flow {
 
 class DelayedPrinterJob extends Flow {
   @override
-  FutureOr<Flow> run(FlowContext context) {
+  Flow run(FlowContext context) {
     return Delayed(
       duration: Duration(seconds: 2),
       next: PrinterJob(),
